f18d46b838b6f5dcb35a89b13336cf77dc1518a2
rjnfrazao <40456619+rjnfrazao@users.noreply.github.com>
2021-02-25 12:12:26 +0300

    Added DTOs

---
+++ TaskCreatePayload.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAPI.DataTransferObjects
+{
+    /// <summary>
+    /// The Task creation or update structure. Custom validation is implemented to validate some fields.
+    /// </summary>
+    /// <remarks>
+    /// Custom validations:
+    /// 1) Due date in the right format.
+    /// 2) Due date in the future.
+    /// 3) taskName not null or blank.
+    /// </remarks>
+    [CustomValidation(typeof(TaskCreatePayload), "TaskPayloadValidation")]
+
+    public class TaskCreatePayload
+    {
+
+        /// <summary>
+        /// Gets or sets the task's name.
+        /// </summary>
+        /// <value>The name.</value>
+        [Required( ErrorMessage = "3")]
+        [StringLength(100, ErrorMessage = "2")]
+        public string taskName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer's email address.
+        /// </summary>
+        /// <value>The email address.</value>
+        [Required( ErrorMessage = "3")]
+        public bool isCompleted{ get; set; }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>The age.</value>
+        [Required( ErrorMessage = "3")]
+        public String dueDate { get; set; }
+
+
+        /// <summary>
+        /// Validates the task name and due date.
+        /// </summary>
+        /// <param name="task">The task.</param>
+        /// <param name="ctx">The context which contains the actual value from the field that the custom validator is validating.</param>
+        /// <returns>ValidationResult.</returns>
+        public static ValidationResult TaskPayloadValidation(TaskCreatePayload task, ValidationContext ctx)
+        {
+            // Verify that the task name in not null nor empty 
+            string taskName = task.taskName;
+            DateTime dueDate;
+            var errorResponse = new ErrorResponse();
+
+
+            // Verify if the date is in the right format
+            try
+            { 
+                dueDate = DataConversion.ToDateTime(task.dueDate); 
+            }
+            catch // (FormatException ex)
+            {
+                return new ValidationResult("9", new List<string> { "dueDate" }); 
+            }
+            
+            // Verify if the task name is null or empty.
+            if (taskName == null || taskName.Length < 1)
+            {
+                return new ValidationResult("3", new List<string> { "taskName" });
+            }
+
+            // Verify that due date is in the present or future.
+            if (dueDate < DateTime.Now)
+            {
+                return new ValidationResult("8", new List<string> { "dueDate"});
+            }
+
+            return ValidationResult.Success;
+
+               
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"taskName=[{taskName}], isCompleted=[{isCompleted}], dueDate=[{dueDate}]";
+        }
+
+
+    }
+}
---
+++ TaskResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Task = TaskAPI.Models.Task;
+
+namespace TaskAPI.DataTransferObjects
+{
+    /// <summary>
+    /// Defines the public facing for the task result, when new task is created or updated.
+    /// </summary>
+    public class TaskResult
+    {
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskResult"/> class using a task as input.
+        /// </summary>
+        /// <param name="task">The customer.</param>
+        public TaskResult(Task task)
+        {
+            id = task.id ?? -1;
+            isCompleted = task.isCompleted;
+            taskName = task.taskName;
+            dueDate = DataConversion.ToDateTimeString(task.dueDate);
+        }
+
+        /// <summary>
+        /// Gets or sets the task identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the task's name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string taskName { get; set; }
+
+        /// <summary>
+        /// Get or set if task is completed.
+        /// </summary>
+        /// <value>Boolean</value>
+        public bool isCompleted { get; set; }
+
+        /// <summary>
+        /// The date the task is due to be completed.
+        /// </summary>
+        /// <value>The date.</value>
+        public string dueDate { get; set; }
+
+    }
+}
